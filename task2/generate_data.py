b632ca3e3a536cffc2766a3aecb0b0467fa49b6d
Peter Ashwell <pash4408@uni.sydney.edu.au>
2011-03-25 11:31:53 +1100

    moved kitty file to models.py

--- task2/generate_data.py
+++ task2/generate_data.py
@@ -13,78 +13,97 @@ import math
 import sys
 import getopt
 import random
-from generateLightCurves import *
+from models import *
 
 def write2file(time, flux, fnum, type):
 	fname = str(type) + "_" + str(fnum) + ".data"
 	f = open(fname, 'w')
+	#print flux
 	for i in xrange(len(time)):
+		#print time[i], flux[i]
 		line = str(time[i]) + "\t" + str(flux[i]) + "\n"
 		f.write(line)
 	f.close()
 
 # generate the test data using Kitty's script (modified slightly)  and the given counts and time domain
-def generateLCData(ttype, amount, total_time, gap_percent = 0):
-	print ttype, amount
-	gap_epochs = int(floor(total_time * gap_percent))
+def generateLCData(ttype, amount, epochs, step, gap_percent):
 	for i in xrange(amount):
-		length = total_time
+		event_length = None
 		time, flux = None, None
 		if ttype == 'SNe':
-			length = int(floor(random.uniform(10, 50))) # TODO: how long does a supernovae last for typically
-			time, flux = generateSupernovae(length)
+			#print "making sne"
+			event_length = int(floor(random.uniform(200, 500))) # TODO: how long does a supernovae last (weeks to months)
+			time, flux = generateSupernovae(event_length)
 		elif ttype == 'ESE':
-			length = int(floor(random.uniform(10, 50))) # TODO: how long do ese last
-			time, flux = generateESE(length)
+			#print "making ese"
+			event_length = int(floor(random.uniform(200, 500))) # TODO: how long do ese last
+			time, flux = generateESE(event_length)
 		elif ttype == 'IDV':
-			length = int(floor(random.uniform(10, 50))) # TODO: should IDV be included in this
-			time, flux = generateIDV(length)
-		else: # if NT or whatever, leave length as default
-			pass # sinusoid will be generated below
-		nt_time, nt_flux = generateSinusoid(total_time)
-		if length != total_time: # if just sinusoid, write to file
-			time_offset = int(floor(random.uniform(0, total_time - length)))
-			for epoch in xrange(time_offset, length):
-				nt_flux[epoch] = flux[epoch]
-		if gap_epochs != 0: # TODO how should gaps be inserted
-			gapstr = bin(random.getrandbits(gap_epochs))[2:]
-			for bit in gapstr:
-				if bit == '1':
-					nt_flux = 'x' # TODO how to indicate gaps
-		write2file(nt_time, nt_flux, i, ttype)	
+			time, flux = generateIDV(event_length)
+		elif ttype == 'NT':
+			pass # An underlying NT is generated for every j
+		# generate a noise curve and superimpose an event on it if we want one
+		nt_time, nt_flux = generateNT(epochs, 1, 0.2) # TODO: suitable mean for non interesting sources 
+		if event_length is not None:
+			event_start = int(floor(random.uniform(0, epochs - event_length)))
+			for epoch in xrange(event_start, event_length):
+				nt_flux[epoch] = flux[epoch] # copy the event onto the uninteresting source curve
+		# calculate the number of missing data points from the desired missing percentage
+		gap_epochs = int(floor(epochs * gap_percent * 0.01))
+		#if gap_epochs != 0:
+		#	gapstr = bin(random.getrandbits(gap_epochs))[2:]
+		#	for bitnum in xrange(len(gapstr)):
+		#		if gapstr[bitnum] == '1':
+		#			nt_flux[bitnum] = 'x' # how to handle gaps
+		if step != 1:
+			nt_time = range(int(floor(1.0 * epochs / step)), step)
+			sampled_ts = [nt_flux[epoch] for epoch in nt_time]
+			nt_flux = sampled_ts
+		# finally, apply the power law distribution to the flux array
+		
+		write2file(nt_time, nt_flux, i, ttype)
 
 def usage():
-	print "usage: generate_data [-eghinst] [-e --ESE ese_count] [-g --gap gap_percent] [-i --IDV idv_count] [-h --help] \
-	[-n --NT nontransient_count] [-s --SNe supernovae_count] [-t epoch_time]"
+	print "usage: generate_data [-eghinstj] [-e --ESE ese count] [-m --missing data missing percent] [-i --IDV idv count] [-h --help] \
+	[-n --NT nontransient count] [-s --SNe supernovae count] [-t epochs] [-j --jump]"
 
 if __name__ == "__main__":
 	desired = {}
-	_epochs = None
+	epochs = None
+	step = None
 	gap_percent = 0
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hs:i:e:n:g:t:", ["help", "SNe", "ESE", "IDV", "NT", "gap"])
-	except getopt.GetoptError:
+		opts, args = getopt.getopt(sys.argv[1:], "hs:i:e:n:m:t:j", ["help", "SNe", "ESE", "IDV", "NT", "missing", "time", "jump"])
+	except getopt.GetoptError as (errno, strerror):
+		print "input error", strerror
 		usage()
 		sys.exit(2)
 	if len(opts) == 0:
+		print "no commands entered"
 		usage()
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
 			usage()
 		elif opt == "-t":
-			_epochs = arg
+			epochs = int(arg)
 		elif opt in ("-s", "--SNe"):
-			desired['SNe'] = arg
+			desired['SNe'] = int(arg)
 		elif opt in ("-e" "--ESE"):
-			desired['ESE'] = arg
+			desired['ESE'] = int(arg)
 		elif opt in ("-i", "--IDV"):
-			desired['IDV'] = arg
+			desired['IDV'] = int(arg)
 		elif opt in ("-n", "--NT"):
-			desired['NT'] = arg
-		elif opt in ("-g", "--gap"):
-			gap_percent = arg
-	if _epochs is None:
-		_epochs = 1000
+			desired['NT'] = int(arg)
+		elif opt in ("-m", "--missing"):
+			gap_percent = float(arg)
+		elif opt in ("s", "--step"):
+			step = int(arg)
+	if epochs is None:
+		epochs = 1000
 		print "set default LC length (1000)"
+	if step is None:
+		step = 1
+		print "set default step size (1)"
 	for ttype in desired.keys():
-		generateLCData(ttype, int(desired[ttype]), int(_epochs), float(gap_percent))
+		generateLCData(ttype, desired[ttype], epochs, step, gap_percent)
+		
