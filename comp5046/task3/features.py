cfcd63eb4420d75a65ec72b3a810f7a02a9e4ae0
Peter Ashwell <pash4408@uni.sydney.edu.au>
2011-05-19 13:02:54 +1000

    Latest version - almost done just needs debugging

--- comp5046/task3/features.py
+++ comp5046/task3/features.py
@@ -23,3 +23,20 @@ class ptFeature:
 					vector[class_offset[tag_star[index]] + feature_offset + self.tag_offset[prev_tag]] -= 1
 					vector[class_offset[tag[index]] + feature_offset + self.tag_offset[prev_tag]] += 1
 
+class cwFeature:
+	def __init__(self, wordlist_file):
+		self.id = "curword_feature"
+		self.word_offset = {}
+		wordlist = open(wordlist_file)
+		for index, word in enumerate(wordlist):
+			self.word_offset[word] = index
+		self.size = len(self.word_offset.keys())
+
+	def score(sentence, tag, word_pos, prev_tag, vector, feature_offset, class_offset):
+		return vector[class_offset[tag] + feature_offset + self.word_offset[sentence[word_pos]]]
+	
+	def update_sentence(sentence, tag_star, tag, vector, feature_offset, class_offset):
+		for index, word in enumerate(sentence):
+			if tag_star[index] != tag[index]:
+				vector[class_offset[tag_star[index]] + feature_offset + self.word_offset[word]] -= 1
+				vector[class_offset[tag[index]] + feature_offset + self.word_offset[word]] += 1
--- comp5046/task3/run_tests.py
+++ comp5046/task3/run_tests.py
@@ -1,9 +1,49 @@
 import random
-
 from perceptron import Perceptron
+from features.cw_feature import cwFeature
+from features.pt_feature import ptFeature
+
+ROUNDS = 1
 
 # Step 1 - load up features into featureset
+cwf = cwFeature("wordlist")
+ptf = ptFeature("taglist")
+fs = FeatureSet([cwf, ptf])
+taglist = open("taglist")
+tags = taglist.read().strip().split('\n')
+percept = Perceptron(fs, tags)
 
 # Step 2 - train perceptron for however many rounds on training data
+training_data = open("data/eng.train")
+sentence = []
+true_tags = []
+for r in xrange(ROUNDS):
+	for line in training_data:
+		line = line.strip()
+		if line == '':
+			percept.train(sentence, tags)
+			sentence = []
+			true_tags = []
+		else:
+			line = line.split(' ')
+			sentence.append(line[0])
+			true_tags.append(line[-1])
+	training_data.seek(0)
+training_data.close()
 
 # Step 3 - classify
+test_data = open("data/eng.testa")
+output = open("results/eng.testa_tested", 'w')
+test_lines = []
+for line in test_data:
+	line = line.strip()
+	if line == '':
+		tags_star = percept.test([obj[0] for obj in test_lines])
+		for tl, tt in izip(test_lines, tags_star):
+			output.write(' '.join(tl) + ' ' + tt)
+		test_lines = []
+	else:
+		line = line.split(' ')
+		test_lines.append(line)
+	
+
